a0d5d25114bc80834426a55f8e38b6e47cd2f3fc
flo <flo33.lav@gmail.com>
2019-01-21 16:27:40 +0100

    regexp

--- Generics.java
+++ Generics.java
@@ -29,7 +29,7 @@ public class Generics {
         for (Integer i: ints) System.out.println(i);
 
         // TODO - Ne retourner que les Strings contenant au moins 2 lettres t.
-        strs = filter( strs, s -> s.toLowerCase().matches(".{2,}t.*") );
+        strs = filter( strs, s -> s.toLowerCase().matches(".*[t].*[t].*") );
         // TODO - Passer en majuscule toutes les Strings
         strs = map(strs, String::toUpperCase);
         for (String s: strs) System.out.println(s);
